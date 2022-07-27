0f5de6fcc8a2dc3ec7be8d4ea08ddc82d78aabfe
sushaanpatel <sushaanpatel@gmail.com>
2022-07-27 19:35:25 +0530

    adding extension code

---
+++ src.js
@@ -0,0 +1,21 @@
+async function getmeme(){
+    let rand = Math.floor(Math.random() * 3);
+    console.log(rand);
+    var subs = ['dankmemes', 'memes', 'wholesomememes'];
+    var title = document.getElementById("title");
+    var image = document.getElementById("image");
+    let r = new XMLHttpRequest();
+    title.innerHTML = "Loading Meme..."; 
+    image.src = "placeholder.gif";
+    r.open("GET", `https://meme-api.herokuapp.com/gimme/${subs[rand]}`, true);
+    r.send();
+    r.onload = () => {
+        let meme = JSON.parse(r.responseText);
+        title.innerHTML = meme.title;
+        image.src = meme.url;
+    }
+}
+
+var btn = document.getElementById('btn');
+btn.addEventListener('click', getmeme);
+window.addEventListener('load', getmeme);
